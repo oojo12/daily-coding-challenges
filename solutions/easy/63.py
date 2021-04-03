82afe7c28105eac543233b364c20abc0214382bf
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-04-03 12:33:04 -0400

    Update 63.py

--- solutions/easy/63.py
+++ solutions/easy/63.py
@@ -5,10 +5,8 @@ def find_target_word(mat, target):
         for col in range(len(mat[0])):
             col_words[col] += mat[row][col]
             row_word += mat[row][col]
-        if row_word == target:
-            return True
+            if row_word == target:
+                return True
+            if col_words[col] == target:
+                return True
     
-    for word in col_words:
-        if word == target:
-            return True
-    return False
