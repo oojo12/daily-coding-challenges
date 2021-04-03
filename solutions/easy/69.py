18a30d6e649d8ebd06491184b6385dc0273e5839
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-04-03 13:00:40 -0400

    Update 69.py

--- solutions/easy/69.py
+++ solutions/easy/69.py
@@ -1,4 +1,4 @@
-#FIX FAILS 4TH ASSERTION
+# update algorithm does not work for all cases
 def find_max_of_three(item_list):
     item_list = sorted(item_list)
     if item_list[-1] * item_list[-2] > item_list[0]*item_list[1]:
@@ -9,4 +9,5 @@ def find_max_of_three(item_list):
 assert 500 == find_max_of_three([-10, -10, 5, 2])
 assert 2000 == find_max_of_three([-20, -10, 5, 10])
 assert 1000 == find_max_of_three([-20, 10, 10, 10])
-assert -1000 == find_max_of_three([-20, -10, -10, 10])
+assert 2000 == find_max_of_three([-20, -10, -10, 10])
+assert -2000 == find_max_of_three([20, -10, -10, 10])
