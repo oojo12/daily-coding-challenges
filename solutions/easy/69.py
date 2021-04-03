a5ab35df1751412a780af445a2ee1ad811787c6c
oojo12 <47154698+oojo12@users.noreply.github.com>
2021-04-03 13:09:16 -0400

    fixed

--- solutions/easy/69.py
+++ solutions/easy/69.py
@@ -1,10 +1,14 @@
-# update algorithm does not work for all cases
-def find_max_of_three(item_list):
-    item_list = sorted(item_list)
-    if item_list[-1] * item_list[-2] > item_list[0]*item_list[1]:
-        return item_list[-1] * item_list[-2] * item_list[-3]
+def find_max_of_three(item):
+    item = sorted(item)
+    # case 1: most negative > most positive and max is non-negative
+    if item[0] * item[1] > item[-1] * item[-2] and item[-1] >= 0:
+        return item[0] * item[1] * item[-1]
+    # case 2: most negative > most positive and max is negative
+    elif item[0] * item[1] > item[-1] * item[-2] and item[-1] < 0:
+        return item[-1] * item[-2] * item[-3]
+    # case 3: most negative < most positive
     else:
-        return item_list[0] * item_list[1] * item_list[-1]
+        return item[-1] * item[-2] * item[-3]
 
 assert 500 == find_max_of_three([-10, -10, 5, 2])
 assert 2000 == find_max_of_three([-20, -10, 5, 10])
